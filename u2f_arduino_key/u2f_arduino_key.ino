1e12a1eeb4fa3c85c9c121f6f1c45b48911da92b
Jakub SQTX Sitarczyk <sssqtx@gmail.com>
2023-11-12 14:04:09 +0100

    Move button control function into separate file

--- u2f_arduino_key/u2f_arduino_key.ino
+++ u2f_arduino_key/u2f_arduino_key.ino
@@ -9,6 +9,7 @@
 #include "sha1.h"
 #include "TOTP.h"
 // My file:
+#include "src/controller.h"
 #include "src/dataConverter.h"
 #include "src/dataController.h"
 
@@ -16,10 +17,10 @@
 // ******************************************************************
 //! Configuration:
 //TODO: Add description
-constexpr int8_t CONTROL_BTN_PIN {7};
+constexpr uint8_t CONTROL_BTN_PIN {7};
+constexpr uint8_t BTN_LOOP_COOLDOWN {15};   // Second-click-loop delay
 constexpr uint16_t WAIT_FOR_ANOTHER_CLICK {500};       // Time how long sys will be waiting to detected second click
 constexpr uint16_t HOW_LONG_PRESS_BTN {1100};       // Time how long sys will be waiting to detected second click
-constexpr uint8_t BTN_LOOP_COOLDOWN {15};   // Second-click-loop delay
 
 /*! The hour difference between the time zone set to RTC and the UTC time zone [in hours]:
  * CET(summer_time) - UTC = 2 [h]
@@ -48,15 +49,9 @@ RTClib myRTC;
 
 void serialFlush();
 
-enum {
-  NONE = -1,
-  POWEROFF = 0,
-  GENERATE_TOKEN,
-  CHOOSE_KEY,
-  ADD_NEW
-};
 
-int btnDetector();
+
+//int btnDetector();
 
 void generateToken();
 
@@ -90,17 +85,17 @@ void setup() {
 
 
 void loop() {
-  switch(btnDetector()){
-    case GENERATE_TOKEN:
+  switch(Controller::btnDetector(CONTROL_BTN_PIN, BTN_LOOP_COOLDOWN, WAIT_FOR_ANOTHER_CLICK, HOW_LONG_PRESS_BTN)){
+    case Controller::GENERATE_TOKEN:
       generateToken();
       break;
-    case CHOOSE_KEY:
+    case Controller::CHOOSE_KEY:
       Serial.println("Choose");
       break;
-    case ADD_NEW:
+    case Controller::ADD_NEW:
       Serial.println("Add");
       break;
-    case POWEROFF:
+    case Controller::POWEROFF:
       Serial.println("Power off");
       break;
     default:
@@ -111,43 +106,7 @@ void loop() {
 }
 
 
-int btnDetector() {
-  bool btnTimerStarted{false};
-  unsigned long startTime{0};
 
-  Serial.println("Use button");
-
-  while(digitalRead(CONTROL_BTN_PIN) == HIGH) {};   // Waiting for first click
-
-  // First click detected
-  if(digitalRead(CONTROL_BTN_PIN) == LOW) {
-    startTime = {millis()};
-    btnTimerStarted = {true};
-    while(digitalRead(CONTROL_BTN_PIN) == LOW) {};
-
-    unsigned long pressTime = {millis() - startTime};
-    if(pressTime >= HOW_LONG_PRESS_BTN) {
-      return ADD_NEW;   // Click and press
-    }
-
-    while(btnTimerStarted) {
-      unsigned long currTime = {millis() - startTime};
-      if(currTime > WAIT_FOR_ANOTHER_CLICK) break;
-
-      // Second click detected
-      if(digitalRead(CONTROL_BTN_PIN) == LOW) {
-        while(digitalRead(CONTROL_BTN_PIN) == LOW) {};
-        return CHOOSE_KEY;  // Double click
-      }
-
-      delay(BTN_LOOP_COOLDOWN);
-    }
-
-    return GENERATE_TOKEN;  // Single click
-  }
-
-  return NONE;  // None click
-}
 
 
 
